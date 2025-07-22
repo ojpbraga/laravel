fb40e091661e2819b63549e75c2a1cd1170ac64a
João Pedro O. Braga <ojpbraga@gmail.com>
2025-07-22 18:47:46 -0300

    class: layout components

---
+++ class-03/resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    
+    <?php // echo $slot ?>
+    <!-- Podemos usar tudo o que esta dentro sera um echo do php -->
+    {{ $slot }}
+
+</body>
+</html>
