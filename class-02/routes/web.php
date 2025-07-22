e800e9bc8f120c06664db931f2077e8fb4571145
João Pedro O. Braga <ojpbraga@gmail.com>
2025-07-22 18:08:59 -0300

    class: views

--- class-02/routes/web.php
+++ class-02/routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return 'About page';
+});
+
+// Podemos retornar outros tipo de dados, como um array
+Route::get('/data', function () {
+    return ['data' => '25/09/2000', 'isSet' => true];
+});
+Route::get('/test', function () {
+    return view('test');
+});
