38fe9f353a73a345917b603c69483c1da6094f60
João Pedro O. Braga <ojpbraga@gmail.com>
2025-08-14 00:25:00 -0300

    class: Migrations

---
+++ class-08/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+Route::get('/', function () {
+    return view('welcome', [
+        'greeting' => 'Hello World',
+        'name' => 'Lary',    
+    ]);
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs' => Job::all()]);
+});
+Route::get('/jobs/{id}', function ($id) {
+    // Illuminate\Support\Arr::first($jobs, function ($job) use ($id) {
+    //     return $job['id'] == $id;
+    // });
+    // dd($job);
+    // $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
+    
+    return view('job', ['job' => $job]);
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
