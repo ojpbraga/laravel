a76cba8f50cbe84f099a112fd874ac6fbbd3fc74
João Pedro O. Braga <ojpbraga@gmail.com>
2025-08-19 23:45:28 -0300

    class: Eloquent

---
+++ class-09/app/Models/Job.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model {
+    protected $table = 'job_listings';
+
+    
+}
+
+?>
